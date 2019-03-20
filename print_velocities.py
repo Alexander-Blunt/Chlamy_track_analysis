5e14fde9e4ee2d707dc0928fde78940cddb23994
aeb553 <aeb553@york.ac.uk>
2019-03-20 11:38:20 +0000

    Renamed output_speed to output_velocity and adjusted the function to output an array instead of a file

--- print_velocities.py
+++ print_velocities.py
@@ -30,24 +30,21 @@ def calc_speed(time1,time2,pos1,pos2):
 #==============================================================================
 # Function to output speed data to file
 #==============================================================================
-def output_speed( input_file )
-    output_file = input_file.replace('track','velocity')
-
+def output_velocity( input_file )
     # Read in the input file, and get the number of points in the track
     track = np.loadtxt(input_file)
     num_points = track.shape[0]
 
-    #Initialise output array column layout: time vx vy vz speed
+    # Initialise output array column layout: time vx vy vz speed
     velocity = np.zeros( ((num_points-1),5) )
 
     for i in range(0,num_points-1):
-        velocity[i,0] = (track[i,0] + track[i+1,0])/2
+        velocity[i,0] = (track[i,0] + track[i+1,0])/2 # calculate time
         for j in range(1,4): # Find speed in each direction from splined track
-            velocity[i,j] = calc_speed(track[i,0],track[i+1,0],track[i,j+3],\
+            velocity[i,j] = calc_speed(track[i,0],track[i+1,0],track[i,j+3],
                                        track[i+1,j+3])
         velocity[i,4] = 
                 (velocity[i,1]**2 + velocity[i,2]**2 + velocity[i,3]**2)**0.5
 
-    np.savetxt(output_file,velocity,fmt='%10.10g')
-
+    return velocity
 #EOF
