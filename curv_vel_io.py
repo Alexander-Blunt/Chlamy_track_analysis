70c9c673e0533d75d03429b2f621c00482f8ba56
aeb553 <aeb553@york.ac.uk>
2019-03-20 12:20:51 +0000

    Program now also outputs velocities

--- curv_vel_io.py
+++ curv_vel_io.py
@@ -13,11 +13,11 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 #==============================================================================
-
-# A program to read in tracks and output curvature and velocity.
-
+# A program to read in tracks and output curvature and velocity to files.
+#==============================================================================
 import numpy as np
 import spiral_analysis as sa
+import velocity_calculation as vc
 from pathlib import Path
 
 # Read in the list of paths to search for tracks
@@ -26,23 +26,37 @@ in_path = np.loadtxt(path_list, dtype=str, skiprows=1)
 
 # Search paths for tracks
 for i in range(0, in_path.size):
-    track_paths = np.array(sorted(Path(in_path[i]).glob('track*.txt')))
+    track_paths = np.array( sorted( Path(in_path[i]).glob('track*.txt') ) )
 
-    # Determine output path for curvature and velocity
+    # Create array to store output paths for each track
     out_path = np.empty(track_paths.shape, dtype=str)
 
-    # Determine output directory and create if it doesn't exist
-    directory = Path(in_path[i]).parent.parent.joinpath('curvature')
-    Path(directory).mkdir(parents=False, exist_ok=True)
+    # Determine curvature output directory and create if it doesn't exist
+    curv_dir = Path(in_path[i]).parent.parent.joinpath('curvature')
+    Path(curv_dir).mkdir(parents=False, exist_ok=True)
 
-    # Output curvatures for all tracks in current path
+    # Determine velocity output directory and create if it doesn't exist
+    vel_dir = Path(in_path[i]).parent.parent.joinpath('velocity')
+    Path(vel_dir).mkdir(parents=False, exist_ok=True)
+
+    #==========================================================================
+    # Output curvatures and velocities for all tracks in current path
+    #==========================================================================
     for track in track_paths:
-        file_name = Path(track).name
-        file_name = file_name.replace('track', 'curv')
-        out_path = Path(directory).joinpath(file_name)
+        track_name = Path(track).name
+        file_name = track_name.replace('track', 'curv')
+        out_path = Path(curv_dir).joinpath(file_name)
 
         # Calculate curvatures and output to file
         curvature = sa.output_curv(track)
         np.savetxt(Path(out_path), curvature, fmt='%10.10g')
+
+        #Output velocities
+        file_name = track_name.replace('track', 'velocity')
+        out_path = Path(vel_dir).joinpath(file_name)
+
+        # Calculate velocities and output to file
+        velocity = vc.output_velocity(track)
+        np.savetxt(Path(out_path), velocity, fmt='%10.10g')
 
 #EOF
